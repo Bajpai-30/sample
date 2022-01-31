d9391a9e08d4f6e48a5195e53725c835a0fe8cb6
Saumya <saumya303bajpai@gmail.com>
2022-01-31 22:31:10 +0530

    chatApp

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const { SSL_OP_NO_TICKET } = require('constants')
+const { Socket } = require('dgram')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+let count = 0
+
+//server(emit) -> client(receive) - countUpdated
+//client(emit) -> server(receive)- increment
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        //socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
